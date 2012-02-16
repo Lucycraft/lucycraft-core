ee162bf2e724c58f01380fd4e6989af8337d1a9e
Wintein Boris <boris.wintein@telenet.be>
2012-02-16 21:45:53 +0100

    GIT GUI test.

---
+++ rewrite.php
@@ -0,0 +1,17 @@
+<?php
+
+    /**
+     * LUCYCRAFT-CORE
+     *
+     * Written by Boris Wintein & Glenn Latomme.
+     *
+     * This is a core website system for setting up websites for Minecraft-servers. It however depends on
+     * Bukkit (www.bukkit.org) and it's plugin system. Without a Minecraft-server with a user managment plugin
+     * this system loses a lot of
+     */
+
+
+
+
+
+?>
